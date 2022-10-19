1e387d8a8544e595c0dec362d9432d43d99baf83
bennkemboi <benkemboi23@gmail.com>
2022-10-19 05:15:42 -0700

    commit print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - start
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}

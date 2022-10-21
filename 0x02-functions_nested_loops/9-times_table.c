09ee706df20e7e5f1e623becf270c0382f8b24e5
bennkemboi <benkemboi23@gmail.com>
2022-10-21 08:37:02 -0700

    add function print 9 timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 		_putchar('0');
 	{
-		for (multi = 0; multi <= 9; multi++)
+		for (multi = 1; multi <= 9; multi++)
 		{
 			_putchar(',');
 		_putchar(' ');

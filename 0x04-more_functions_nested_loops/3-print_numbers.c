ff782f062f83db973210875d79da481181a6e0f4
bennkemboi <benkemboi23@gmail.com>
2022-10-21 14:26:01 -0700

    commit print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * print_numbers - print nums fro 0 to 9
- *
+ *print_numbers - print nums from 0 to 9
  *Return: void
  *
  */
 void print_numbers(void)
 {
-	char c;
+	char c; 
 
 	for (c = '0'; c <= '9'; c++)
 	{

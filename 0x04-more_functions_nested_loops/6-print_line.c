c3a63b7286fe97583a83ac0d92c2af22122fc6ec
bennkemboi <benkemboi23@gmail.com>
2022-10-24 01:35:06 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,18 +9,11 @@
 
 void print_line(int n)
 {
-	int a = 0;
+	int a;
 
-	if (n > 0)
+	for (a = 0; a < n; a++)
 	{
-		while (a <= n)
-		{
-			_putchar('_');
-			a++;
-		}
-		_putchar('\n');
+		_putchar('_');
 	}
-	else
-		_putchar('\n');
+	_putchar('\n');
 }
-

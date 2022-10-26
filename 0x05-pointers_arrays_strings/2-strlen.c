2ae84cb25cffaf91c9520921c95549579a6f693d
bennkemboi <benkemboi23@gmail.com>
2022-10-26 13:15:48 -0700

    define length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strlen - return the length of string
+ *@s: string
+ *Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*S != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}

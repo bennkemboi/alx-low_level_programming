139cd3f5327cf416c6ced82c1ed0dbd963cba4bb
bennkemboi <benkemboi23@gmail.com>
2022-11-24 00:50:28 -0800

    add dog basic info

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,27 +1,30 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
+
 /**
- * struct dog- dog info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- * Description: Third member
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-/**
- * *dog_t -typede for struct dog
- */
-typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int_strlen(char *s);
 
 #endif

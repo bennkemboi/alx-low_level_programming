b1051a0d6c09bb18abae6f1f68e49b164afdc923
bennkemboi <benkemboi23@gmail.com>
2022-12-06 10:34:02 -0800

    add task 0 update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,17 +6,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int val = 0;
-  int i = 0;
+int i;
+	unsigned int j;
 
-  if (b == NULL)
-    return 0;
-
-  while (b[i] == '0' || b[i] == '1')
-  {  // Found another digit.
-    val <<= 1;
-    val += b[i]-'0';
-    i++;
-  }
-  return val;
+	j = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
